d811532d30e108f6de98d5f79de91f37900516ef
marufuu <almaruf58@gmail.com>
2026-04-16 21:24:00 +0600

    Initial Commit

---
+++ FirebaseNote/FirebaseNoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseNoteApp.swift
+//  FirebaseNote
+//
+//  Created by Maruf on 16/4/26.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseNoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
